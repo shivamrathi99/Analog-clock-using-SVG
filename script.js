672786cf66afb7243a890f21df3a473feaf77b6c
shivamrathi99 <rathishivam99@gmail.com>
2020-12-30 01:28:36 +0530

    solved the reset at 0 error

--- script.js
+++ script.js
@@ -2,18 +2,23 @@ const HOURHAND = document.querySelector("#hour");
 const MINUTEHAND = document.querySelector("#minute");
 const SECONDHAND = document.querySelector("#second");
 
+var date = new Date();
+console.log(date);
+let hr = date.getHours();
+let min = date.getMinutes();
+let sec = date.getSeconds();
+console.log("Hour: " + hr + " Minute: " + min + " Second: " + sec);
+
+let hrPosition = (hr * 360) / 12 + (min * (360 / 60)) / 12;
+let minPosition = (min * 360) / 60 + (sec * (360 / 60)) / 60;
+let secPosition = (sec * 360) / 60;
+
 function runTheClock() {
-  var date = new Date();
-  console.log(date);
-  let hr = date.getHours();
-  let min = date.getMinutes();
-  let sec = date.getSeconds();
-  console.log("Hour: " + hr + " Minute: " + min + " Second: " + sec);
-
-  let hrPosition = (hr * 360) / 12 + (min * (360 / 60)) / 12;
-  let minPosition = (min * 360) / 60 + (sec * (360 / 60)) / 60;
-  let secPosition = (sec * 360) / 60;
-
+  // added to have a 1 second delay for the animation so it does not reset
+  //it will manually get the time and then have the interval set
+  hrPosition = hrPosition + 3 / 360;
+  minPosition = minPosition + 6 / 60;
+  secPosition = secPosition + 6;
   HOURHAND.style.transform = "rotate(" + hrPosition + "deg)";
   MINUTEHAND.style.transform = "rotate(" + minPosition + "deg)";
   SECONDHAND.style.transform = "rotate(" + secPosition + "deg)";
